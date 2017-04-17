88178c37849c4f7bcd15e80b2d8ff6f07e6a4297
ecrux <estebancruz373@gmail.com>
2017-04-17 07:11:04 -0500

    Creación de mi primer comit
    Detectar si existe la función.

---
+++ detectar_existencia.php
@@ -0,0 +1,18 @@
+<?PHP
+	if (function_exists('mysqli_connect')) 
+	{
+		//mysqli está instalado
+		echo "Si";
+	}else{
+		echo "No";
+	}
+
+	echo "<br><br>";
+
+	//Esta otra función se encontró en http://stackoverflow.com/questions/16254413/php-5-installed-also-mysqli-is-installed-but-not-compiling-in-mac-lion
+	echo "extension_loaded mysqli ".extension_loaded('mysqli');
+
+	echo "<br><br>";
+
+	echo phpinfo();
+?>
